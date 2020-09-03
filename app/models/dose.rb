f80091155baadca134d0a1355b8b146381c3c767
Mathieu Longe <mathieu.longe@orange.fr>
2020-09-03 12:31:17 +1000

    done

--- app/models/dose.rb
+++ app/models/dose.rb
@@ -1,6 +1,7 @@
 class Dose < ApplicationRecord
   belongs_to :cocktail
   belongs_to :ingredient
+
   validates :description, presence: true
   validates :ingredient, uniqueness: { scope: :cocktail, message: "This cocktail and this ingredient have already been created together" }
 end

b80a2de5dfd74b9cffa65fb288a74e3472b8ad0f
Mathieu Longe <mathieu.longe@orange.fr>
2020-10-26 14:55:36 +1100

    dashboard done

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,8 +5,9 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   validates :username, presence: true, uniqueness: true
 
-  has_many :reviews
-  has_many :cocktails
+  has_many :cocktails, dependent: :destroy
+  has_many :reviews, through: :cocktails
+
 
   def can_review?(cocktail)
     cocktail.reviews.none? { |review| review.user_id == self.id }
